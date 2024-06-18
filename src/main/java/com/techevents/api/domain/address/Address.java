04fc3b6e1fa9a3f0e60c81dac6d324cfb97f89e5
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-06-18 17:46:18 +0100

    feat: create entities

---
+++ src/main/java/com/techevents/api/domain/address/Address.java
@@ -0,0 +1,27 @@
+package com.techevents.api.domain.address;
+
+import com.techevents.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String city;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
